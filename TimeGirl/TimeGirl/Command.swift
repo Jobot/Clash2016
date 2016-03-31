19a8684b4462853aa6be538cc6a30c5cf12a1186
Joseph W. Dixon <jwdixon@gmail.com>
2016-03-30 22:17:24 -0500

    now allowing synonyms

--- TimeGirl/TimeGirl/Command.swift
+++ TimeGirl/TimeGirl/Command.swift
@@ -14,12 +14,25 @@ enum Command {
     static func commandFromText(rawText: String) -> (command: Command?, remainingText: String) {
         let text = rawText.lowercaseString
         
-        if text.hasPrefix("examine ") {
-            let index = rawText.startIndex.advancedBy(8)
+        let examine = text.beginsWithPrefixInList([ "examine", "look at" ])
+        if examine.hasPrefix {
+            let prefix = examine.prefix!
+            let index = rawText.startIndex.advancedBy(prefix.characters.count + 1)
             let remainingText = rawText.substringFromIndex(index)
             return (.Examine, remainingText)
         }
         
         return (nil, rawText)
+    }
+}
+
+extension String {
+    func beginsWithPrefixInList(prefixes: [String]) -> (hasPrefix: Bool, prefix: String?) {
+        for prefix in prefixes {
+            if self.hasPrefix("\(prefix) ") {
+                return (true, prefix)
+            }
+        }
+        return (false, nil)
     }
 }
