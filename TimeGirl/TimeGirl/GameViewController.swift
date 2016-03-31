91676b97de419761b9895937117b6971e65935a1
Joseph W. Dixon <jwdixon@gmail.com>
2016-03-31 16:11:17 -0500

    sets up base storyboard

---
+++ TimeGirl/TimeGirl/GameViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GameViewController.swift
+//  TimeGirl
+//
+//  Created by Joseph Dixon on 3/31/16.
+//  Copyright © 2016 Joseph W. Dixon. All rights reserved.
+//
+
+import Cocoa
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
