4f0518451070b63b629f9f7869cecbacc79e8acc
Joseph W. Dixon <jwdixon@gmail.com>
2016-03-30 19:28:24 -0500

    adds empty command and parser

---
+++ TimeGirl/TimeGirl/Command.swift
@@ -0,0 +1,13 @@
+//
+//  Command.swift
+//  TimeGirl
+//
+//  Created by Joseph Dixon on 3/30/16.
+//  Copyright © 2016 Joseph W. Dixon. All rights reserved.
+//
+
+import Foundation
+
+protocol Command {
+    
+}
---
+++ TimeGirl/TimeGirl/TextParser.swift
@@ -0,0 +1,15 @@
+//
+//  TextParser.swift
+//  TimeGirl
+//
+//  Created by Joseph Dixon on 3/30/16.
+//  Copyright © 2016 Joseph W. Dixon. All rights reserved.
+//
+
+import Foundation
+
+struct TextParser {
+    func parseCommandFromText(text: String) -> Command? {
+        return nil
+    }
+}
