8664960616ecdaac0c7605dc2c852004f6b6c447
Joseph W. Dixon <jwdixon@gmail.com>
2016-03-30 19:20:57 -0500

    created project

---
+++ TimeGirl/TimeGirl/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  TimeGirl
+//
+//  Created by Joseph Dixon on 3/30/16.
+//  Copyright © 2016 Joseph W. Dixon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+}
+
