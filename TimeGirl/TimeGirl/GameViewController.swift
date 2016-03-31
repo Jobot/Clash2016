448f05616635de2bc94307ed1fc908af4cda06bb
Joseph W. Dixon <jwdixon@gmail.com>
2016-03-31 17:05:40 -0500

    we now have basic interaction!

--- TimeGirl/TimeGirl/GameViewController.swift
+++ TimeGirl/TimeGirl/GameViewController.swift
@@ -14,11 +14,16 @@ class GameViewController: NSViewController, NSTextFieldDelegate {
     @IBOutlet var textView: NSTextView!
     @IBOutlet var textField: NSTextField!
     
+    var parser: TextParser!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
         
         textField.delegate = self
+        textField.placeholderString = "What do you want to do now?"
+        
+        parser = TextParser()
     }
     
     override func controlTextDidEndEditing(obj: NSNotification) {
@@ -28,6 +33,37 @@ class GameViewController: NSViewController, NSTextFieldDelegate {
         
         let text = textField.stringValue
         textField.stringValue = ""
-        print("You typed: \(text)")
+        processText(text)
+    }
+    
+    func processText(text: String) {
+        appendMessage(text, toTextView: textView)
+        guard let command = parser.parseCommandFromText(text) else {
+            appendMessage("I do not understand.", toTextView: textView)
+            return
+        }
+        processCommand(command)
+    }
+    
+    func processCommand(command: Command) {
+        var message: String
+        switch command {
+        case let .Examine(item):
+            message = "You do not see \(item) here."
+        case let .Take(item):
+            message = "You cannot take \(item)."
+        case let .Open(item):
+            message = "You cannot seem to open \(item)."
+        }
+        
+        appendMessage(message, toTextView: textView)
+    }
+    
+    func appendMessage(message: String, toTextView textView: NSTextView) {
+        guard let string = textView.string else {
+            return
+        }
+        
+        textView.string = "\(string)\n\(message)"
     }
 }
